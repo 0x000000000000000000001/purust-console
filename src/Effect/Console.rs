a8e66c9a30b05685b29843f0b5fad61ac18aa2dd
0x1 <kevin.francart3@gmail.com>
2026-08-11 21:22:13 +0200

    misc

--- src/Effect/Console.rs
+++ src/Effect/Console.rs
@@ -1,15 +1,1 @@
 
-pub fn Effect_Console_warn() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Effect_Console_timeLog() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Effect_Console_timeEnd() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Effect_Console_time() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Effect_Console_info() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Effect_Console_groupEnd() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Effect_Console_groupCollapsed() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Effect_Console_group() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Effect_Console_debug() -> crate::UnknownType { crate::UnknownType::new(0) }
-pub fn Effect_Console_clear() -> crate::UnknownType { crate::UnknownType::new(0) }
-
-pub fn Effect_Console_error() -> crate::UnknownType { crate::UnknownType::new(0) }
-
-pub fn Effect_Console_log() -> crate::UnknownType { crate::UnknownType::new(0) }
