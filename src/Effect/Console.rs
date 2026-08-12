efd7585d2fce28e88170791e38c79042f968130c
0x1 <kevin.francart3@gmail.com>
2026-08-12 09:18:39 +0200

    misc

--- src/Effect/Console.rs
+++ src/Effect/Console.rs
@@ -1,1 +1,19 @@
+pub fn Effect_Console_log(s: String) -> crate::UnknownType {
+    println!("{}", s);
+    crate::UnknownType::new(Record_a { ..Default::default() })
+}
 
+pub fn Effect_Console_warn(s: String) -> crate::UnknownType {
+    eprintln!("WARN: {}", s);
+    crate::UnknownType::new(Record_a { ..Default::default() })
+}
+
+pub fn Effect_Console_error(s: String) -> crate::UnknownType {
+    eprintln!("ERROR: {}", s);
+    crate::UnknownType::new(Record_a { ..Default::default() })
+}
+
+pub fn Effect_Console_info(s: String) -> crate::UnknownType {
+    println!("INFO: {}", s);
+    crate::UnknownType::new(Record_a { ..Default::default() })
+}
