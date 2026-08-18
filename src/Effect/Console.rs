7452512d25019f45ba2fb056f50f6f1010e5f456
0x1 <kevin.francart3@gmail.com>
2026-08-18 20:54:25 +0200

    misc

--- src/Effect/Console.rs
+++ src/Effect/Console.rs
@@ -1,52 +1,115 @@
-pub fn Effect_Console_log(mut a0: crate::UnknownType) -> crate::UnknownType {
-    println!("{}", a0.init_string.as_ref().unwrap());
-    crate::UnknownType::new(crate::Record_a { ..Default::default() })
+use std::rc::Rc;
+use crate::{UnknownType, Record_a};
+
+pub fn Effect_Console_log(mut a0: UnknownType) -> UnknownType {
+    perceus_ptr::PerceusPtr::new(Record_a {
+        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+            println!("{}", a0.init_string.as_ref().unwrap());
+            UnknownType::new(Record_a { ..Default::default() })
+        })),
+        ..Default::default()
+    })
 }
 
-pub fn Effect_Console_error(mut a0: crate::UnknownType) -> crate::UnknownType {
-    eprintln!("{}", a0.init_string.as_ref().unwrap());
-    crate::UnknownType::new(crate::Record_a { ..Default::default() })
+pub fn Effect_Console_error(mut a0: UnknownType) -> UnknownType {
+    perceus_ptr::PerceusPtr::new(Record_a {
+        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+            eprintln!("{}", a0.init_string.as_ref().unwrap());
+            UnknownType::new(Record_a { ..Default::default() })
+        })),
+        ..Default::default()
+    })
 }
 
-pub fn Effect_Console_warn(mut a0: crate::UnknownType) -> crate::UnknownType {
-    eprintln!("WARN: {}", a0.init_string.as_ref().unwrap());
-    crate::UnknownType::new(crate::Record_a { ..Default::default() })
+pub fn Effect_Console_warn(mut a0: UnknownType) -> UnknownType {
+    perceus_ptr::PerceusPtr::new(Record_a {
+        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+            eprintln!("WARN: {}", a0.init_string.as_ref().unwrap());
+            UnknownType::new(Record_a { ..Default::default() })
+        })),
+        ..Default::default()
+    })
 }
 
-pub fn Effect_Console_info(mut a0: crate::UnknownType) -> crate::UnknownType {
-    println!("INFO: {}", a0.init_string.as_ref().unwrap());
-    crate::UnknownType::new(crate::Record_a { ..Default::default() })
+pub fn Effect_Console_info(mut a0: UnknownType) -> UnknownType {
+    perceus_ptr::PerceusPtr::new(Record_a {
+        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+            println!("INFO: {}", a0.init_string.as_ref().unwrap());
+            UnknownType::new(Record_a { ..Default::default() })
+        })),
+        ..Default::default()
+    })
 }
 
-pub fn Effect_Console_debug(mut a0: crate::UnknownType) -> crate::UnknownType {
-    println!("DEBUG: {}", a0.init_string.as_ref().unwrap());
-    crate::UnknownType::new(crate::Record_a { ..Default::default() })
+pub fn Effect_Console_debug(mut a0: UnknownType) -> UnknownType {
+    perceus_ptr::PerceusPtr::new(Record_a {
+        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+            println!("DEBUG: {}", a0.init_string.as_ref().unwrap());
+            UnknownType::new(Record_a { ..Default::default() })
+        })),
+        ..Default::default()
+    })
 }
 
-pub fn Effect_Console_clear() -> crate::UnknownType {
-    crate::UnknownType::new(crate::Record_a { ..Default::default() })
+pub fn Effect_Console_clear() -> UnknownType {
+    perceus_ptr::PerceusPtr::new(Record_a {
+        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+            UnknownType::new(Record_a { ..Default::default() })
+        })),
+        ..Default::default()
+    })
 }
 
-pub fn Effect_Console_group(mut a0: crate::UnknownType) -> crate::UnknownType {
-    crate::UnknownType::new(crate::Record_a { ..Default::default() })
+pub fn Effect_Console_group(mut a0: UnknownType) -> UnknownType {
+    perceus_ptr::PerceusPtr::new(Record_a {
+        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+            UnknownType::new(Record_a { ..Default::default() })
+        })),
+        ..Default::default()
+    })
 }
 
-pub fn Effect_Console_groupCollapsed(mut a0: crate::UnknownType) -> crate::UnknownType {
-    crate::UnknownType::new(crate::Record_a { ..Default::default() })
+pub fn Effect_Console_groupCollapsed(mut a0: UnknownType) -> UnknownType {
+    perceus_ptr::PerceusPtr::new(Record_a {
+        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+            UnknownType::new(Record_a { ..Default::default() })
+        })),
+        ..Default::default()
+    })
 }
 
-pub fn Effect_Console_groupEnd() -> crate::UnknownType {
-    crate::UnknownType::new(crate::Record_a { ..Default::default() })
+pub fn Effect_Console_groupEnd() -> UnknownType {
+    perceus_ptr::PerceusPtr::new(Record_a {
+        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+            UnknownType::new(Record_a { ..Default::default() })
+        })),
+        ..Default::default()
+    })
 }
 
-pub fn Effect_Console_time(mut a0: crate::UnknownType) -> crate::UnknownType {
-    crate::UnknownType::new(crate::Record_a { ..Default::default() })
+pub fn Effect_Console_time(mut a0: UnknownType) -> UnknownType {
+    perceus_ptr::PerceusPtr::new(Record_a {
+        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+            UnknownType::new(Record_a { ..Default::default() })
+        })),
+        ..Default::default()
+    })
 }
 
-pub fn Effect_Console_timeEnd(mut a0: crate::UnknownType) -> crate::UnknownType {
-    crate::UnknownType::new(crate::Record_a { ..Default::default() })
+pub fn Effect_Console_timeEnd(mut a0: UnknownType) -> UnknownType {
+    perceus_ptr::PerceusPtr::new(Record_a {
+        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+            UnknownType::new(Record_a { ..Default::default() })
+        })),
+        ..Default::default()
+    })
 }
 
-pub fn Effect_Console_timeLog(mut a0: crate::UnknownType) -> crate::UnknownType {
-    crate::UnknownType::new(crate::Record_a { ..Default::default() })
+pub fn Effect_Console_timeLog(mut a0: UnknownType) -> UnknownType {
+    perceus_ptr::PerceusPtr::new(Record_a {
+        call: Some(Rc::new(move |mut _u: UnknownType| -> UnknownType {
+            UnknownType::new(Record_a { ..Default::default() })
+        })),
+        ..Default::default()
+    })
 }
